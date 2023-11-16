5d099f87fc6e22de8c5a29d26612f18b9e44ba1d
Abraamyan Alexander <hryapusek@gmail.com>
2023-11-16 22:40:40 +0300

    new: simple_reply_to_message function
    This allow to avoid code duplication

--- commands_utils.py
+++ commands_utils.py
@@ -1,5 +1,16 @@
+import vk_api
+
 def is_command(text: str, command: str) -> bool:
     if text is None:
         return False
     msg_split = text.strip().split()
     return len(msg_split) != 0 and text.strip().split()[0] == ("/" + command)
+
+def simple_reply_to_message(vk_session: vk_api.VkApi, peer_id: int, message_id: int, text: str):
+    method_params = {
+            "random_id": 0,
+            "peer_id": peer_id,
+            "reply_to": message_id,
+            "message": text,
+        }
+    vk_session.method("messages.send", method_params)
